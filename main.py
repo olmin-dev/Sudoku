4391a0a9aff1b87eae92bb600d504cc89eabddc3
Bertin Clement <Clement.Bertin@bordeaux-inp.fr>
2019-12-19 18:30:53 +0100

    problem with the rules error detector

--- main.py
+++ main.py
@@ -20,10 +20,38 @@ def printSudokuCase(a):
     for i in range(3):
         print(a[i])
 
+# Return True if the number already exist in the square
+def isAlreadyInSquare(a, test):
+    for i in range(3):
+        for j in range(3):
+            if(a[i][j] == test):
+                return True
+    return False
+
+# Return True if the number already exist in the square
+def isAlreadyInColumn(a, test, x, y, n):
+    for i in range(3):
+        for j in range(3):
+            if(a[i*3 + n][x][j] == test):
+                return True
+    return False
+
+# Return True if the number already exist in the square
+def isAlreadyInLine(a, test, x, y, n):
+    for i in range(3):
+        for j in range(3):
+            if(a[(n // 3) + i][j][y] == test):
+                return True
+    return False
+
+# Return True if the number already exist in the square
+def isAlreadyIn(a, test, x, y, n):
+    return not(isAlreadyInSquare(a[n], test)) and not(isAlreadyInLine(a, test, x, y, n)) and not(isAlreadyInColumn(a, test, x, y, n))
+
 # Return True if the proposition is correct (according the sudoku's rules)
 def isCorrect(a, test, n, x, y):
     for i in range(len(a)):
-        if(a[n][i][x] == test or a[n][y][i] == test):
+        if(not(isAlreadyIn(a[i], test, x, y, n))):
             return False
     return True
 
@@ -49,7 +77,7 @@ def backtrackingSolver(a):
     toSolve = toSolve(a)
     for (i, j, k) in toSolve:
         for l in range(9):
-            if(isCorrect(a, l, i, j, k):
+            if(isCorrect(a, l, i, j, k)):
                 addNumber(a, l, i, j, k)
 
 # Board example TODO : Search a real sudoku board
@@ -66,3 +94,5 @@ test = [[[1,2,3],[4,5,6],[7,8,0]],
 printSudoku(test)
 
 print(toSolve(test))
+
+print(isAlreadyIn(test, 5, 0, 1, 2))
